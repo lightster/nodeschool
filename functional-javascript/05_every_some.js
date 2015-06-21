d50e574374dee5728b664bfb2b07958938de37e8
Matt Light <matt.light@illuminateed.com>
2015-06-20 19:59:58 -0700

    Implement "Every Some" lesson

---
+++ functional-javascript/05_every_some.js
@@ -0,0 +1,11 @@
+'strict';
+
+module.exports = function (goodUsers) {
+    return function (submittedUsers) {
+        return submittedUsers.every(function (submittedUser) {
+            return goodUsers.some(function (goodUser) {
+                return goodUser.id == submittedUser.id
+            })
+        })
+    }
+}
