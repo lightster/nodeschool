bae06ea55cd3b11be1da14091b5143ca3476c900
Matt Light <matt.light@illuminateed.com>
2015-06-22 08:32:00 -0700

    Make "partial application without bind" more functional

--- functional-javascript/09_partial_application_without_bind.js
+++ functional-javascript/09_partial_application_without_bind.js
@@ -4,8 +4,6 @@ var slice = Array.prototype.slice
 
 module.exports = function logger(namespace) {
     return function () {
-        var args = slice.call(arguments)
-        args.unshift(namespace)
-        console.log.apply(null, args)
+        console.log.apply(null, [namespace].concat(slice.call(arguments)))
     }
 }
