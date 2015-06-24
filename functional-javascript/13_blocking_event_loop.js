155dc97b80985054eb18e30dc17e66e0feef1ef9
Matt Light <matt.light@illuminateed.com>
2015-06-24 00:43:01 -0700

    Fix formatting of lesson

--- functional-javascript/13_blocking_event_loop.js
+++ functional-javascript/13_blocking_event_loop.js
@@ -1,12 +1,16 @@
 'use strict';
 
 function repeat(operation, num) {
-  if (num <= 0) return
-  operation()
-  setTimeout(
-    repeat.bind(null, operation, --num),
-    1
-  )
+    if (num <= 0) {
+        return
+    }
+
+    operation()
+
+    setTimeout(
+        repeat.bind(null, operation, --num),
+        1
+    )
 }
 
 module.exports = repeat
