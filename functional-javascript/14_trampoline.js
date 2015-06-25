29591b4a948bd6fa63c9916f83f7f4ad9eed07a3
Matt Light <matt.light@illuminateed.com>
2015-06-25 07:50:07 -0700

    Update trampoline lesson to not use recursion

--- functional-javascript/14_trampoline.js
+++ functional-javascript/14_trampoline.js
@@ -1,15 +1,9 @@
 'use strict';
 
 function repeat(operation, num) {
-    if (num <= 0) {
-        return
-    } else if (num > 100) {
-        repeat(operation, 100)
-        return repeat.bind(null, operation, num - 100)
-    } else {
-        operation()
-        repeat(operation, --num)
-    }
+    if (num <= 0) return
+    operation()
+    return repeat.bind(null, operation, --num)
 }
 
 function trampoline(fn) {
