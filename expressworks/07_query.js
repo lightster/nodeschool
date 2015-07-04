936f8aa4657bb6b1f163ff432bbef482d60f1b1a
Matt Light <matt.light@illuminateed.com>
2015-07-04 00:24:38 -0700

    Implement "what's in query" lesson

---
+++ expressworks/07_query.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var express = require('express')
+var app = express()
+
+app.get('/search', function (request, response) {
+    response.send(request.query)
+})
+app.listen(process.argv[2])
