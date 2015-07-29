e78ac2ee8919a59987e6947d3ada8aef74a90e01
Matt Light <matt.light@illuminateed.com>
2015-07-29 07:32:13 -0700

    Implement "do some work" lesson

---
+++ promise-it-wont-hurt/11_do_some_work.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var http = require('q-io/http');
+
+http.read('http://localhost:7000')
+    .then(function(sessionId) {
+        http.read('http://localhost:7001/' + sessionId)
+            .then(function(userJson) {
+                console.log(JSON.parse(userJson.toString()));
+            })
+            .then(null, console.error.bind(console))
+            .done();
+    })
+    .then(null, console.error.bind(console))
+    .done();
