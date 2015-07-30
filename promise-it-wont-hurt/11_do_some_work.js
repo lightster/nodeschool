7b7ed531dc05708467be549aacb164b3c7f1fc91
Matt Light <matt.light@illuminateed.com>
2015-07-30 08:03:56 -0700

    Unnest promises and depend on chaining
    Duh. Part of the reason promises are awesome!

--- promise-it-wont-hurt/11_do_some_work.js
+++ promise-it-wont-hurt/11_do_some_work.js
@@ -4,12 +4,10 @@ var http = require('q-io/http');
 
 http.read('http://localhost:7000')
     .then(function(sessionId) {
-        http.read('http://localhost:7001/' + sessionId)
-            .then(function(userJson) {
-                console.log(JSON.parse(userJson.toString()));
-            })
-            .then(null, console.error.bind(console))
-            .done();
+        return http.read('http://localhost:7001/' + sessionId);
+    })
+    .then(function(userJson) {
+        console.log(JSON.parse(userJson.toString()));
     })
     .then(null, console.error.bind(console))
     .done();
--- promise-it-wont-hurt/12_more_functional.js
+++ promise-it-wont-hurt/12_more_functional.js
@@ -5,10 +5,8 @@ var _ = require('lodash');
 
 http.read('http://localhost:7000')
     .then(function(sessionId) {
-        http.read('http://localhost:7001/' + sessionId)
-            .then(_.flowRight(console.log, JSON.parse))
-            .then(null, console.error.bind(console))
-            .done();
+        return http.read('http://localhost:7001/' + sessionId);
     })
+    .then(_.flowRight(console.log, JSON.parse))
     .then(null, console.error.bind(console))
     .done();
