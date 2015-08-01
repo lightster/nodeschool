df405ea1d8cdb96f78a1db938a737e3d662adbf2
Matt Light <matt.light@illuminateed.com>
2015-07-31 23:55:19 -0700

    Work towards cleaner solution to "more functional" lesson

--- promise-it-wont-hurt/12_more_functional.js
+++ promise-it-wont-hurt/12_more_functional.js
@@ -5,7 +5,8 @@ var _ = require('lodash');
 
 http.read('http://localhost:7000')
     .then(function(sessionId) {
-        return http.read('http://localhost:7001/' + sessionId);
+        var getSessionUrl = _.bind(String.prototype.concat, 'http://localhost:7001/');
+        return http.read(getSessionUrl(sessionId));
     })
     .then(_.flowRight(console.log, JSON.parse))
     .then(null, console.error.bind(console))
